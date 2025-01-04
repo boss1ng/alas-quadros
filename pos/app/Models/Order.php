5963ff595f0bd43b4af5d8037e039643b1633223
Joey Bryan Pintor <joeybryan_pintor@yahoo.com>
2025-01-04 20:46:19 +0800

    Created a model. migrations, and initial order controller

---
+++ pos/app/Models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_name',
+        'orders',
+        'total_price',
+    ];
+
+    protected $casts = [
+        'orders' => 'array', // Automatically cast JSON to array
+    ];
+}
