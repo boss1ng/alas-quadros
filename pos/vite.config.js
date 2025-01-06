a90f148b6c677e78e73199fbf83a1db108cb6b81
Bryan Pintor <jpintor@you-source.com>
2025-01-06 21:13:44 +0800

    Enable project to run locally and accessible in public

--- pos/vite.config.js
+++ pos/vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: true, // Enables external access
+        hmr: {
+            host: '192.168.1.25', // Your local IP
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
