64ade8dfef073524e1ebeb910b687e646293f590
Bryan Pintor <jpintor@you-source.com>
2025-01-13 11:34:57 +0800

    Land to orders when cook instead of dashboard

--- pos/resources/views/welcome.blade.php
+++ pos/resources/views/welcome.blade.php
@@ -31,12 +31,19 @@
                         @if (Route::has('login'))
                             <nav class="-mx-3 flex flex-1 justify-end">
                                 @auth
-                                    <a
-                                        href="{{ url('/dashboard') }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                    >
-                                        Dashboard
-                                    </a>
+                                    @if (Auth::user()->role === "admin")
+                                        <a
+                                            href="{{ url('/dashboard') }}"
+                                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        >
+                                            Dashboard
+                                        </a>
+                                    @elseif (Auth::user()->role === "cook")
+                                        <a href="{{ url('/order') }}"
+                                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                                            Orders
+                                        </a>
+                                    @endif
                                 @else
                                     <a
                                         href="{{ route('login') }}"
