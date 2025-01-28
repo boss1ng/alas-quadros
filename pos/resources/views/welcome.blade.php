7c2e5cb6996212d5101a1799bca3638a1f7920bf
Joey Bryan Pintor <joeybryan_pintor@yahoo.com>
2025-01-28 20:00:13 +0800

    If cashier and cook, Orders

--- pos/resources/views/welcome.blade.php
+++ pos/resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
                                         >
                                             Dashboard
                                         </a>
-                                    @elseif (Auth::user()->role === "cook")
+                                    @elseif (Auth::user()->role === "cook" || Auth::user()->role === "cashier")
                                         <a href="{{ url('/order') }}"
                                             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                                             Orders
